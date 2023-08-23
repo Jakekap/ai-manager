8920b00d5b3e12d242b73d47ffe947e9bcf51e57
Christian Andrade <ce.andrade.p@gmail.com>
2023-08-23 17:24:51 -0500

    first commit

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+
+export default function DashboardPage() {
+  return (
+    (<>
+    <p className='text-6xl text-green-500'>DashboardPage (protected)</p>
+    
+    </>
+    )
+  )
+}
